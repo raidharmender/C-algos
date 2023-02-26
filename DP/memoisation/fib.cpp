57e31a70a4118515da3c0cba7dbfbf874f0f87f7
Dharmender Rai <dharmender.rai@gmail.com>
2023-02-26 23:18:40 +0800

    added new code

--- DP/memoisation/fib.cpp
+++ DP/memoisation/fib.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+vector<int> memo;
 int counter = 0;
 int fib(int n) {
     counter++;
+    if(memo[n] != -1) return memo[n];
     if (n < 3) {
         return 1;
     }
-    return fib(n - 1) + fib(n - 2);
+    return memo[n] = fib(n - 1) + fib(n - 2);
 }
 
 int main() {
-    int x;
+    int x;    
     cin >> x;    
+    memo.resize(x + 1, -1);
     cout << fib(x)  << ":" << counter << endl;
     return 0;
 }
